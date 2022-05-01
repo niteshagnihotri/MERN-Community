b5622ac529396570afc7fd4fb3cf4badf82624ae
nitesh agnihotri <niteshagni1830@gmail.com>
2022-05-01 20:33:28 +0530

    changes undone

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const DB = process.env.DB;
-mongoose.connect('mongodb+srv://admin:root@portfolio.j71m2.mongodb.net/portfolio?retryWrites=true&w=majority')
+const DB = process.env.DB;
+mongoose.connect(DB)
 .then(()=>console.log("Connection Successfull"))
 .catch((err)=>console.log(err));
