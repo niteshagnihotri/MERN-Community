c377517c1073bd1f7ff5619b1167d4d032ffb7eb
nitesh agnihotri <niteshagni1830@gmail.com>
2022-05-01 20:30:27 +0530

    db updated

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB = process.env.DB;
-mongoose.connect(DB)
+// const DB = process.env.DB;
+mongoose.connect('mongodb+srv://admin:root@portfolio.j71m2.mongodb.net/portfolio?retryWrites=true&w=majority')
 .then(()=>console.log("Connection Successfull"))
 .catch((err)=>console.log(err));
